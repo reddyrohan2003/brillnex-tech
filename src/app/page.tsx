254aeed3bea2c36ad009b493846923415a2c51f8
reddyrohan2003 <Rohanyeruva2003@gmail.com>
2026-06-26 19:15:08 +0530

    remove core values, reorder sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from "@/components/home/HeroSection";
 import HiringPartners from "@/components/home/HiringPartners";
 import AboutSection from "@/components/home/AboutSection";
-import CoreValues from "@/components/home/CoreValues";
 import WhyChooseUs from "@/components/home/WhyChooseUs";
 import AchievementCounters from "@/components/home/AchievementCounters";
 import TrendingCourses from "@/components/home/TrendingCourses";
@@ -16,10 +15,9 @@ export default function Home() {
       <HiringPartners />
       <TrendingCourses />
       <PricingPlans />
-      <AboutSection />
       <AchievementCounters />
-      <CoreValues />
       <WhyChooseUs />
+      <AboutSection />
       <LeadFormFAQ />
       <Newsletter />
     </>
